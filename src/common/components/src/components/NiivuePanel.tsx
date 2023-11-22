a91ab4afc302cad3efc0cc5566270bba1668b7a5
Yuelong Li <yuelong.edu@gmail.com>
2023-11-21 23:16:10 -0800

    niivue listens for location changes

--- src/common/components/src/components/NiivuePanel.tsx
+++ src/common/components/src/components/NiivuePanel.tsx
@@ -21,57 +21,24 @@ interface NiivuePanelProps{
     mms: number[];
 }
 
-function createGUI(){
-    const element = document.getElementById('controlDock');
-    if (element?.firstChild) {
-        element.innerHTML='';
-    }
-    const gui = new GUI({
-        container: (document.getElementById( 'controlDock' )as HTMLElement) }) ;
 
-    const customStyleTag = document.createElement( 'style' );
-    document.head.appendChild( customStyleTag );
-    customStyleTag.innerHTML=`.lil-gui {
-            --background-color: #ffffff;
-            --text-color: #3d3d3d;
-            --title-background-color: #efefef;
-            --title-text-color: #3d3d3d;
-            --widget-color: #f0f0f0;
-            --hover-color: #f0f0f0;
-            --focus-color: #fafafa;
-            --number-color: #000000;
-            --string-color: #8da300;
-        }
-        
-        .lil-gui .controller > .name{
-            font-size:12pt;
-        }
-        .lil-gui .title {
-            font-size:14pt;
-        }
-        `;
-    const myObject = {
-        xSlice: 0,
-        ySlice: 1,
-        zSlice: 2,
-    };
-    let controllerX=gui.add( myObject, 'xSlice',0,1 );   // Number Field
-    let controllerY=gui.add( myObject, 'ySlice' ,0,1);   // Number Field
-    let controllerZ=gui.add( myObject, 'zSlice' ,0,1);   // Number Field
-    return {gui,controllerX,controllerY,controllerZ};
+function toRatio(val:number,min:number,max:number){
+    return (val-min)/(max-min);
 }
+
 export function NiivuePanel (props:NiivuePanelProps) {
     const sliceControl = React.useRef(null);
 	const canvas = React.useRef(null)
     const histogram = React.useRef<HTMLElement>(null);
     const {mins,maxs,mms} = props;
+    const {gui,controllerX,controllerY,controllerZ} = createGUI();
+
     let height = 650;
     // This hook is for initialization, called only once
     React.useEffect(()=>{
         props.nv.attachTo('niiCanvas');
         props.nv.opts.dragMode=props.nv.dragModes.pan;
     },[canvas]);
-
     // This hook is called when show distribution state changed
 	React.useEffect(() => {
         props.nv.resizeListener();
@@ -86,17 +53,48 @@ export function NiivuePanel (props:NiivuePanelProps) {
         // histogram.current?.addEventListener('resize',()=>props.resampleImage());
     }, [histogram]);
 
-    let {gui,controllerX,controllerZ,controllerY} = createGUI();
-
-    controllerX.min(mins[0]).max(maxs[0]).setValue(mms[0]);
-    controllerY.min(mins[1]).max(maxs[1]).setValue(mms[1]);
-    controllerZ.min(mins[2]).max(maxs[2]).setValue(mms[2]);
+    const [pause, pauseUpdate] = React.useState(false);
+    controllerX.min(mins[0]).max(maxs[0]);
+    controllerY.min(mins[1]).max(maxs[1]);
+    controllerZ.min(mins[2]).max(maxs[2]);
+    controllerX.setValue(mms[0]);
+    controllerY.setValue(mms[1]);
+    controllerZ.setValue(mms[2]);
     controllerX.onChange((val:number)=>{
         console.log(val);
         console.log(props.nv.drawPenLocation);
-        // props.nv.document.scene.crosshairPos[0] = val;
-        props.nv.drawPenLocation = [val,mms[1],mms[2]];
+        let crosshairPos = [toRatio(val,mins[0],maxs[0]),
+                                        toRatio(mms[1],mins[1],maxs[1]),
+                                        toRatio(mms[2],mins[2],maxs[2])];
+        props.nv.scene.crosshairPos = crosshairPos;
+        // The following code are taken from Niivue source to change
+        // crosshair location imperatively, in the future shall be replaced with Niivue
+        // official API if otherwise supported
         props.nv.drawScene();
+        props.nv.createOnLocationChange();
+    })
+    controllerY.onChange((val:number)=>{
+        console.log(val);
+        console.log(props.nv.drawPenLocation);
+        let crosshairPos = [toRatio(mms[0],mins[0],maxs[0]),
+            toRatio(val,mins[1],maxs[1]),
+            toRatio(mms[2],mins[2],maxs[2])];
+        props.nv.scene.crosshairPos = crosshairPos;
+        props.nv.drawScene();
+        // props.nv.createOnLocationChange();
+    })
+    controllerZ.onChange((val:number)=>{
+        console.log(val);
+        console.log(props.nv.drawPenLocation);
+        let crosshairPos = [toRatio(mms[0],mins[0],maxs[0]),
+            toRatio(mms[1],mins[1],maxs[1]),
+            toRatio(val,mins[2],maxs[2])];
+        props.nv.scene.crosshairPos = crosshairPos;
+        // The following code are taken from Niivue source to change
+        // crosshair location imperatively, in the future shall be replaced with Niivue
+        // official API if otherwise supported
+        props.nv.drawScene();
+        // props.nv.createOnLocationChange();
     })
     React.useEffect(()=>{
         document.getElementById('controlDock')?.appendChild(gui.domElement);
@@ -159,3 +157,45 @@ export function NiivuePanel (props:NiivuePanelProps) {
         </Box>
 	)
 }
+
+
+function createGUI(){
+    const element = document.getElementById('controlDock');
+    if (element?.firstChild) {
+        element.innerHTML='';
+    }
+    const gui = new GUI({
+        container: (document.getElementById( 'controlDock' )as HTMLElement) });
+
+    const customStyleTag = document.createElement( 'style' );
+    document.head.appendChild( customStyleTag );
+    customStyleTag.innerHTML=`.lil-gui {
+            --background-color: #ffffff;
+            --text-color: #3d3d3d;
+            --title-background-color: #efefef;
+            --title-text-color: #3d3d3d;
+            --widget-color: #f0f0f0;
+            --hover-color: #f0f0f0;
+            --focus-color: #fafafa;
+            --number-color: #000000;
+            --string-color: #8da300;
+        }
+        
+        .lil-gui .controller > .name{
+            font-size:12pt;
+        }
+        .lil-gui .title {
+            font-size:14pt;
+        }
+        `;
+    const myObject = {
+        xSlice: 0,
+        ySlice: 1,
+        zSlice: 2,
+    };
+    let controllerX=gui.add( myObject, 'xSlice',0,1 );   // Number Field
+    let controllerY=gui.add( myObject, 'ySlice' ,0,1);   // Number Field
+    let controllerZ=gui.add( myObject, 'zSlice' ,0,1);   // Number Field
+    return {gui,controllerX,controllerY,controllerZ};
+}
+
