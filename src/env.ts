e2538d605cc0425cd9f4c1ec0ccc902a0eb6995c
Yuelong Li <yuelong.edu@gmail.com>
2024-04-17 21:53:23 +0800

    update env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,10 +1,10 @@
 export const CLOUDMR_SERVER = 'https://ewjjq013u0.execute-api.us-east-1.amazonaws.com';
 
-export const MRO_SERVER = 'https://dplk0uo9d0.execute-api.us-east-1.amazonaws.com/Prod';
+export const MRO_SERVER = 'https://ry5ib49g2d.execute-api.us-east-1.amazonaws.com/Prod';
 
 //To be merged into CloudMR Server
 export const PROFILE_SERVER = 'https://ewjjq013u0.execute-api.us-east-1.amazonaws.com';
 
 export const TOKEN_URL = 'https://ewjjq013u0.execute-api.us-east-1.amazonaws.com/api-token';
 
-export const API_TOKEN = 'mrotpimum--';
+export const API_TOKEN = 'yPWaWARdLvaKZd0blo3cjBcZBNwZE3t2ghzSs6Rf';
