a2f3305d6c38b658de1ce667d38b83bb21c3570b
Yuelong Li <yuelong.edu@gmail.com>
2024-05-10 19:56:23 +0800

    update variable names and template setup

--- src/env.ts
+++ src/env.ts
@@ -5,6 +5,6 @@ export const MRO_SERVER = 'https://ry5ib49g2d.execute-api.us-east-1.amazonaws.co
 //To be merged into CloudMR Server
 export const PROFILE_SERVER = 'https://ewjjq013u0.execute-api.us-east-1.amazonaws.com';
 
-export const TOKEN_URL = 'https://ewjjq013u0.execute-api.us-east-1.amazonaws.com/api-token';
+export const API_URL = 'https://ewjjq013u0.execute-api.us-east-1.amazonaws.com/api-token';
 
 export const API_TOKEN = 'yPWaWARdLvaKZd0blo3cjBcZBNwZE3t2ghzSs6Rf';
