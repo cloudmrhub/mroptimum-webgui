0855a0eb1dcc7d59a61a4aef6e7f7dd8d409395c
Eros <eros.montin@gmail.com>
2024-12-29 20:31:34 -0500

    dev note

--- src/env.ts
+++ src/env.ts
@@ -1,11 +1,24 @@
-export const CLOUDMR_SERVER = 'https://ewjjq013u0.execute-api.us-east-1.amazonaws.com';
+// export const CLOUDMR_SERVER = 'https://ewjjq013u0.execute-api.us-east-1.amazonaws.com';
 
-export const MRO_SERVER = 'https://ry5ib49g2d.execute-api.us-east-1.amazonaws.com/Prod';
+// export const MRO_SERVER = 'https://ry5ib49g2d.execute-api.us-east-1.amazonaws.com/Prod';
 
-//To be merged into CloudMR Server
-export const PROFILE_SERVER = 'https://ewjjq013u0.execute-api.us-east-1.amazonaws.com';
+// //To be merged into CloudMR Server
+// export const PROFILE_SERVER = 'https://ewjjq013u0.execute-api.us-east-1.amazonaws.com';
 
-export const API_URL = 'https://ewjjq013u0.execute-api.us-east-1.amazonaws.com/api-token';
+// export const API_URL = 'https://ewjjq013u0.execute-api.us-east-1.amazonaws.com/api-token';
 
 
-export const API_TOKEN = 'yPWaWARdLvaKZd0blo3cjBcZBNwZE3t2ghzSs6Rf';
+// export const API_TOKEN = 'yPWaWARdLvaKZd0blo3cjBcZBNwZE3t2ghzSs6Rf';
+
+
+
+export const CLOUDMR_SERVER = process.env.CLOUDMR_SERVER || 'https://ewjjq013u0.execute-api.us-east-1.amazonaws.com';
+
+export const MRO_SERVER = process.env.MRO_SERVER || 'https://ry5ib49g2d.execute-api.us-east-1.amazonaws.com/Prod';
+
+// To be merged into CloudMR Server
+export const PROFILE_SERVER = process.env.PROFILE_SERVER || 'https://ewjjq013u0.execute-api.us-east-1.amazonaws.com';
+
+export const API_URL = process.env.API_URL || 'https://ewjjq013u0.execute-api.us-east-1.amazonaws.com/api-token';
+
+export const API_TOKEN = process.env.API_TOKEN || 'yPWaWARdLvaKZd0blo3cjBcZBNwZE3t2ghzSs6Rf';
